bebdc34a6488503c405e2904e9a721386877f74d
Alexandra Zelik <125389471+AleksandraZelik@users.noreply.github.com>
2024-01-26 12:04:55 +0200

    Update Homework2_task1.java

--- homework2/Homework2_task1.java
+++ homework2/Homework2_task1.java
@@ -5,6 +5,7 @@ public class Homework2_task1 {
 
         double number1 = Double.parseDouble(args[0]);
         double number2 = Double.parseDouble(args[1]);
+        
         double result = Math.pow(number1,number2);
         System.out.println(result);
     }
